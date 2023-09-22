ae63f64d90d94a55e6d9d2813edb10e01f9b0383
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-09-22 13:37:04 -0600

    Examen

---
+++ src/LabExamen/Entry.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LabExamen;
+
+/**
+ *
+ * @author tomea
+ */
+public class Entry {
+    private String user;
+    private long 
+    
+}
